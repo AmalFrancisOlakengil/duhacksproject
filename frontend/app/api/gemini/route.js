951fd98c61ba14f9ab8a242d3326917bf8902624
AmalFrancisOlakengil <francisamal030@gmail.com>
2025-02-23 21:41:55 +0530

    api change

--- frontend/app/api/gemini/route.js
+++ frontend/app/api/gemini/route.js
@@ -4,7 +4,7 @@ export async function POST(request) {
   const { prompt, imageParts } = await request.json();
   
   try {
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
     const result = await model.generateContent([prompt, ...imageParts]);
